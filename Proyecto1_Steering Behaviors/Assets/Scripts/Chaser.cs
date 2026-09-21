51e01316676c4c26f6508479ff9c88c459064f30
Rocio-ux <rociolujansantos@gmail.com>
2026-09-20 22:15:54 -0300

    Script a Chaser
    Agrego script a Chaser, deambula, persigue al jugador con Seek y frena con Arrive al llegar, si lo pierde de vista el vuelve a deambular

--- Proyecto1_Steering Behaviors/Assets/Scripts/Chaser.cs
+++ Proyecto1_Steering Behaviors/Assets/Scripts/Chaser.cs
@@ -2,15 +2,56 @@ using UnityEngine;
 
 public class Chaser : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float wanderSpeed = 5f;     
+    public float turnInterval = 4f;    
+    public float minTurnAngle = 45f;
+    public float maxTurnAngle = 100f;
+    public float detectRadius = 5f;   
+    public float loseRadius = 8f;    
+    public float arriveRadius = 2f;  
+    public float stopRadius = 1f;     
+
+    Rigidbody2D rb;
+    Transform player;
+    Vector2 dir;
+    float nextTurn;
+    bool chasing;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player").transform;
+        dir = Random.insideUnitCircle.normalized;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 toPlayer = (Vector2)player.position - rb.position;
+        float dist = toPlayer.magnitude;
+
+        if (dist < detectRadius) chasing = true;
+        else if (dist > loseRadius) chasing = false;
+
+        if (chasing)   
+            rb.linearVelocity = toPlayer.normalized * wanderSpeed * Mathf.Clamp01((dist - stopRadius) / (arriveRadius - stopRadius));
+        else           
+            Wander();
+    }
+
+    void Wander()
+    {
+        if (Time.time >= nextTurn)
+        {
+            float giro = Random.Range(minTurnAngle, maxTurnAngle) * (Random.value < 0.5f ? -1 : 1);
+            dir = Quaternion.Euler(0, 0, giro) * dir;
+            nextTurn = Time.time + turnInterval;
+        }
+        rb.linearVelocity = dir * wanderSpeed;
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        dir = (rb.position - col.GetContact(0).point).normalized;   
+        nextTurn = Time.time + turnInterval;
     }
 }

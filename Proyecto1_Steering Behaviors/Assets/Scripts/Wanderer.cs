74a64a5c20d6a1def343b70a48e4aba855016fe9
Rocio-ux <rociolujansantos@gmail.com>
2026-09-20 22:07:22 -0300

    Script Wanderer
    Agrego script a Wanderer con movimiento aleatorio y cambios de rumbo cada 5 segundos

--- Proyecto1_Steering Behaviors/Assets/Scripts/Wanderer.cs
+++ Proyecto1_Steering Behaviors/Assets/Scripts/Wanderer.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class Wanderer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float wanderSpeed = 5f;
+    public float turnInterval = 5f;    
+    public float minTurnAngle = 45f;  
+    public float maxTurnAngle = 100f;  
+
+    Rigidbody2D rb;
+    Vector2 dir;                       
+    float nextTurn;                    
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        dir = Random.insideUnitCircle.normalized;   
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Time.time >= nextTurn)
+        {
+            float giro = Random.Range(minTurnAngle, maxTurnAngle) * (Random.value < 0.5f ? -1 : 1);
+            dir = Quaternion.Euler(0, 0, giro) * dir;  
+            nextTurn = Time.time + turnInterval;
+        }
+        rb.linearVelocity = dir * wanderSpeed;
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        dir = (rb.position - col.GetContact(0).point).normalized;   
+        nextTurn = Time.time + turnInterval;
     }
 }
